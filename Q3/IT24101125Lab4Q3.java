6d49b18fe54322558ae7a1ab5af37b0d5e341a4a
IT24101125 <IT24101125@my.sliit.lk>
2024-08-18 23:11:11 +0530

    Add files via upload

---
+++ Q3/IT24101125Lab4Q3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class IT24101125Lab4Q3
+{
+	public static void main(String[] args)
+	{		
+	Scanner values=new Scanner(System.in);
+	int number;
+	String result;
+
+	System.out.print("Enter a number :");
+	number=values.nextInt();
+
+	result=(number > 0) ? "Positive":
+		(number < 0) ? "Negative":
+		"the number is Zero";
+
+	System.out.print(result);
+	}
+ }
